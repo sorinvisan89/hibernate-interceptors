46cb10d4f2ee84fef522d65f965a8a12875c27ce
sorinvisan89 <sorin_andrei_visan@yahoo.com>
2021-10-21 15:45:33 +0300

    added foundation for integration tests

---
+++ src/test/java/com/integration/RestIT.java
@@ -0,0 +1,11 @@
+package com.integration;
+
+import org.junit.jupiter.api.Test;
+
+public class RestIT {
+
+    @Test
+    public void integrationTest1() {
+        System.out.println("integrationTest1");
+    }
+}
